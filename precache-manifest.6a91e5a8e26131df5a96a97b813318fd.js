213e3958f3f05086a0c496c2bb74582a69b1daf7
sunhao <sunhao@localhost.localdomain>
2019-03-07 13:47:03 +0800

    Updates

---
+++ precache-manifest.6a91e5a8e26131df5a96a97b813318fd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a774c87b3a9c6843bcb",
+    "url": "/test_hhca_ui/static/css/main.a91ef0f7.chunk.css"
+  },
+  {
+    "revision": "7a774c87b3a9c6843bcb",
+    "url": "/test_hhca_ui/static/js/main.7a774c87.chunk.js"
+  },
+  {
+    "revision": "841ab2683a6dba311306",
+    "url": "/test_hhca_ui/static/js/runtime~main.841ab268.js"
+  },
+  {
+    "revision": "2b693f5f0d6ba6a01a42",
+    "url": "/test_hhca_ui/static/css/2.d7f7fc61.chunk.css"
+  },
+  {
+    "revision": "2b693f5f0d6ba6a01a42",
+    "url": "/test_hhca_ui/static/js/2.2b693f5f.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_hhca_ui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bd6369ec9e76a9f9fee24fae1c442d66",
+    "url": "/test_hhca_ui/static/media/DNA.bd6369ec.png"
+  },
+  {
+    "revision": "07af3ab33a8dafb9e09729714d26eb81",
+    "url": "/test_hhca_ui/static/media/topics1.07af3ab3.png"
+  },
+  {
+    "revision": "3c6b921b5abc0d2faf04f6682fa2fb61",
+    "url": "/test_hhca_ui/static/media/Explore.3c6b921b.png"
+  },
+  {
+    "revision": "9a858ab202e46072c66c9e11a92ab23a",
+    "url": "/test_hhca_ui/static/media/tsne.9a858ab2.png"
+  },
+  {
+    "revision": "60b7caefe661c48585cdc1c27c82f0fd",
+    "url": "/test_hhca_ui/static/media/irf3mi_1.60b7caef.png"
+  },
+  {
+    "revision": "a99394b2ef7018d532e88170c15ae525",
+    "url": "/test_hhca_ui/static/media/sc-trajectory-olfr.a99394b2.png"
+  },
+  {
+    "revision": "6ed802364b7647eefcdffa6597aa7da4",
+    "url": "/test_hhca_ui/static/media/olgatroyansk.6ed80236.jpg"
+  },
+  {
+    "revision": "5859b2b2a226f1b17d53be0f21582818",
+    "url": "/test_hhca_ui/static/media/heart-showing-anatomic.5859b2b2.png"
+  },
+  {
+    "revision": "85b85b11a07e645a125cb520a9cd5a23",
+    "url": "/test_hhca_ui/static/media/Banner_HCA_1112x410.85b85b11.jpg"
+  },
+  {
+    "revision": "d4ecd83e2b31476fbc55f8a7d39bf21e",
+    "url": "/test_hhca_ui/static/media/41586_2018_590_Fig2_HTML.d4ecd83e.png"
+  },
+  {
+    "revision": "35fa4c8c5197cae8846afb098349d28d",
+    "url": "/test_hhca_ui/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/test_hhca_ui/precache-manifest.6a91e5a8e26131df5a96a97b813318fd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/test_hhca_ui/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
